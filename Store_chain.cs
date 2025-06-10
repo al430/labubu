64a29297af84817b0411f2b7d9e1ef88850f7e9e
al430 <126333805+al430@users.noreply.github.com>
2025-06-10 11:52:50 +0500

    Add files via upload

---
+++ Store_chain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd_3
+{
+    internal class Store_chain: Store_chain_base
+    {
+        public string Name { get; set; }
+        public string Adres { get; set; }
+        public int Count_sales { get; set; }
+        public int Mont_revenue { get; set; }
+        public int Count_buyer { get; set; }
+
+        public Store_chain(string name, string adres, int count_sal, int mont_rev, int count_buyer) : base (name, adres, count_sal, mont_rev)
+        {
+            Name = name;
+            Adres = adres;
+            Count_sales = count_sal;
+            Mont_revenue = mont_rev;
+            Count_buyer = count_buyer;
+        }
+
+        public double Profit()
+        {
+            if (Count_sales > 50000) return 2*base.Profit();
+            return 0.5*base.Profit();
+        }
+
+        public string Info()
+        {
+            return base.Info() + $" {Count_buyer.ToString()}";
+        }
+
+        public override List<Store_chain_base> ListStore()
+        {
+            return base.ListStore();
+        }
+
+        public List<Store_chain_base> SortList()
+        {
+            return base.ListStore().OrderBy(x => x.Name).ToList();
+        }
+
+        public List<Store_chain_base> SortList(ModeSort mod)
+        {
+            return mod switch
+            {
+                ModeSort.Name => base.ListStore().OrderBy(x => x.Name).ToList(),
+                ModeSort.Adres => base.ListStore().OrderBy(x => x.Adres).ToList(),
+                ModeSort.Count_sales => base.ListStore().OrderBy(x => x.Count_sales).ToList(),
+                ModeSort.Mont_revenue => base.ListStore().OrderBy(x => x.Mont_revenue).ToList(),
+                ModeSort.Count_buyer => base.ListStore().OrderBy(x => x.Count_sales).ToList()
+            };
+            
+        }
+    }
+}
---
+++ Store_chain_base.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd_3
+{
+    public class Store_chain_base
+    {
+        public string Name { get; set; }
+        public string Adres { get; set; }
+
+        static List<Store_chain_base> list_store;
+        public int Count_sales {  get; set; }
+        public int Mont_revenue { get; set; }
+
+        public Store_chain_base(string name, string adres, int count_sal, int mont_rev)
+        {
+            Name = name;
+            Adres = adres;
+            Count_sales = count_sal;
+            Mont_revenue = mont_rev;
+
+            list_store.Add(this);
+        }
+
+        public double Profit()
+        {
+            return (double)(Mont_revenue / Count_sales);
+        }
+
+        public string Info()
+        {
+            return $"{Name} {Adres} {Count_sales.ToString()} {Mont_revenue.ToString()}";
+        }
+
+        public virtual List<Store_chain_base> ListStore()
+        {
+            return list_store;
+        }
+    }
+}
